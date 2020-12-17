ecb4566baf7fee5129f96bc1847cddb38b5c4bbc
lucanchling <luc.anchling@cpe.fr>
2020-12-17 08:45:42 +0100

    Menu

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -9,25 +9,33 @@
 from tkinter import Tk, Canvas, Label, Button, Menu
 
 
-# Partie Graphique
+# Partie Graphique :
 
 # Création de la fenetre :
 fenetre = Tk()
 fenetre.title('Space Invaders')
 
-# Zone principale de jeu
+# Zone principale de jeu :
 canevas = Canvas(fenetre, width = 580, height = 420)
 canevas.pack(side = 'left')
 
-# Zone affichant le score
+# Zone affichant le score :
 labelScore = Label(fenetre, text='Score :')
 labelScore.pack(side = 'top')
 
-# Bouton déclenchant la partie
+# Menu :
+menubar = Menu(fenetre)
+menu1 = Menu(menubar, tearoff = 0)
+menu1.add_command(label = 'New Game')
+menu1.add_command(label = 'Quit', command = fenetre.destroy)
+menubar.add_cascade(label = 'Partie', menu = menu1)
+fenetre.config(menu = menubar)
+
+# Bouton déclenchant la partie :
 buttonStart = Button(fenetre, text = 'Start')
 buttonStart.pack()
 
-# Bouton permettant de sortir du jeu
+# Bouton permettant de sortir du jeu :
 buttonQuit = Button(fenetre, text = 'Quit', command = fenetre.destroy)
 buttonQuit.pack()
 
