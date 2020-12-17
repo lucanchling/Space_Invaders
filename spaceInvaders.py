ed85eb0a629a7264d418ae843e147c758185ee06
lucanchling <luc.anchling@cpe.fr>
2020-12-17 10:30:42 +0100

    Déplacement Vaisseau

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -6,29 +6,57 @@
 # To Do :
 
 # Importation des modules :
-from tkinter import *
+from tkinter import Tk, Canvas, Label, Button, Menu
 
-LARGEUR = 480
-HAUTEUR = 320
+Largeur = 480
+Hauteur = 320
 RAYON = 20
-X = LARGEUR/2
-Y = HAUTEUR/2
+X = Largeur/2
+Y = Hauteur/2
 DX = 1
 
+# Fonction déplaçant les aliens
 def deplacement():
-    global X,Y,DX,DY,RAYON,LARGEUR,HAUTEUR
+    Canevas.focus_set()
+    global X,Y,DX,RAYON,Largeur,Hauteur
     # Gestion bord droit
-    if X+RAYON+DX > LARGEUR:
-        X = 2*(LARGEUR-RAYON)-X
+    if X+RAYON+DX > Largeur:
+        X = 2*(Largeur-RAYON)-X
         DX = -DX
     # Gestion bord gauche
     if X-RAYON+DX < 0:
         X = 2*RAYON-X
         DX = -DX
     X = X+DX
-    canevas.coords(alien,X-RAYON,Y-RAYON,X+RAYON,Y+RAYON)
+    Canevas.coords(alien,X-RAYON,Y-RAYON,X+RAYON,Y+RAYON)
     fenetre.after(15,deplacement)
     buttonStart.destroy()
+
+# position initiale du Vaisseau
+PosX = 0.61*Largeur
+PosY = 1.2*Hauteur
+
+def DeplacementVaisseau(event):
+    global PosX,Largeur
+    touche = event.keysym
+    # Vaisseau au milieu
+    if PosX+10 < Largeur and PosX-10 > 0:
+        # Déplacement à droite
+        if touche == 'Right':
+            PosX += 5
+        # Déplacement à Gauche
+        if touche == 'Left':
+            PosX -= 5
+    # Vaisseau sur le bord droit
+    elif PosX+10 == Largeur :
+        if touche == 'Left':
+            PosX -= 5
+    # Vaisseau sur le bord gauche
+    elif PosX-10 == 0:
+        if touche == 'Right':
+            PosX += 5
+        # on dessine le Vaisseau � sa nouvelle position
+    Canevas.coords(Vaisseau,PosX -10, PosY -10, PosX +10, PosY +10)
 
 # Partie Graphique :
 
@@ -37,9 +65,11 @@ fenetre = Tk()
 fenetre.title('Space Invaders')
 
 # Zone principale de jeu :
-LARGEUR,HAUTEUR=580,420
-canevas = Canvas(fenetre, width = LARGEUR, height = HAUTEUR, bg='grey')
-canevas.pack(side = 'left')
+Largeur,Hauteur=580,420
+Canevas = Canvas(fenetre, width = Largeur, height = Hauteur, bg='grey')
+Vaisseau = Canevas.create_rectangle(PosX-10,PosY-10,PosX+10,PosY+10,width=1,outline='black',fill='red')
+Canevas.bind('<Key>',DeplacementVaisseau)
+Canevas.pack(side = 'left')
 
 # Zone affichant le score :
 labelScore = Label(fenetre, text='Score :')
@@ -60,6 +90,8 @@ buttonStart.pack()
 # Bouton permettant de sortir du jeu :
 buttonQuit = Button(fenetre, text = 'Quit', command = fenetre.destroy)
 buttonQuit.pack()
-alien = canevas.create_rectangle(X-RAYON,Y-RAYON,X+RAYON,Y+RAYON,width=1,fill='blue')
+
+# Création de l'objet alien :
+alien = Canevas.create_rectangle(X-RAYON,Y-RAYON,X+RAYON,Y+RAYON,width=1,fill='blue')
 
 fenetre.mainloop()
