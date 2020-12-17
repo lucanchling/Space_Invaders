92c1a1b5a04e90898b59b853d4d846cbb2fb8e2f
lucanchling <luc.anchling@cpe.fr>
2020-12-17 08:35:11 +0100

    Partie graphique : done

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -6,3 +6,29 @@
 # To Do :
 
 # Importation des modules :
+from tkinter import Tk, Canvas, Label, Button, Menu
+
+
+# Partie Graphique
+
+# Création de la fenetre :
+fenetre = Tk()
+fenetre.title('Space Invaders')
+
+# Zone principale de jeu
+canevas = Canvas(fenetre, width = 580, height = 420)
+canevas.pack(side = 'left')
+
+# Zone affichant le score
+labelScore = Label(fenetre, text='Score :')
+labelScore.pack(side = 'top')
+
+# Bouton déclenchant la partie
+buttonStart = Button(fenetre, text = 'Start')
+buttonStart.pack()
+
+# Bouton permettant de sortir du jeu
+buttonQuit = Button(fenetre, text = 'Quit', command = fenetre.destroy)
+buttonQuit.pack()
+
+fenetre.mainloop()
