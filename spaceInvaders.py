f008be09ab745d7d7aec4491456543c0f370f3ed
lucanchling <luc.anchling@cpe.fr>
2021-01-07 11:34:06 +0100

    missile alien - déclench avec une touche

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -7,23 +7,27 @@
 
 # Importation des modules :
 from tkinter import Tk, Canvas, Label, Button, Menu
+from time import time
+from random import randint
 
-Largeur = 580
-Hauteur = 420
+tic = time()
+
+largeur = 580
+hauteur = 420
 RAYON = 20
 delta = 75
-X = [Largeur/2,Largeur/2+delta,Largeur/2-delta,Largeur/2+2*delta,Largeur/2-2*delta,Largeur/2+3*delta,Largeur/2-3*delta]
-Y = Hauteur/2
+X = [largeur/2,largeur/2+delta,largeur/2-delta,largeur/2+2*delta,largeur/2-2*delta,largeur/2+3*delta,largeur/2-3*delta]
+Y = hauteur/2
 DX = 0.5
 
 # Fonction déplaçant les aliens
 def deplacement():
     Canevas.focus_set()
-    global X,Y,DX,RAYON,Largeur,Hauteur,PosY
+    global X,Y,DX,RAYON,largeur,hauteur,PosY
     # Gestion bord droit
     for i in range(len(X)):
-        if X[i]+RAYON+DX > Largeur:
-            X[i] = 2*(Largeur-RAYON)-X[i]
+        if X[i]+RAYON+DX > largeur:
+            X[i] = 2*(largeur-RAYON)-X[i]
             DX = -DX
         # Gestion bord gauche avec desccente de l'alien
         if X[i]-RAYON+DX < 0:
@@ -39,8 +43,8 @@ def deplacement():
 
 
 # position initiale du Vaisseau
-PosX = int(0.5*Largeur)
-PosY = int(0.95*Hauteur)
+PosX = int(0.5*largeur)
+PosY = int(0.95*hauteur)
 tailleVaiss = 15
 
 # Missile :
@@ -48,12 +52,12 @@ tailleMissile = 15
 vitMissile = 3
 
 # Permet de gérer le déplacement du missile
-def Deplacementmissile():
-    global misX,misY,Hauteur,tailleMissile,tailleVaiss,missile
+def deplacementMissile():
+    global misX,misY,hauteur,tailleMissile,tailleVaiss,missile
     misY -= vitMissile
     Canevas.coords(missile,misX,misY,misX,misY+tailleMissile) 
     if misY > 5:
-        fenetre.after(15,Deplacementmissile)
+        fenetre.after(15,deplacementMissile)
     # Collision avec le bord haut
     if misY < 5:
         Canevas.delete(missile)
@@ -65,12 +69,28 @@ def Deplacementmissile():
             del X[i]
             Canevas.delete(missile)
 
+
+# Permet de gérer les missiles des aliens
+def alienMissile():
+    global misAlX,misAlY,misAlien,vitMissile,tailleMissile
+    misAlY += vitMissile
+    Canevas.coords(misAlien,misAlX,misAlY,misAlX,misAlY+tailleMissile)
+    if misAlY < hauteur:
+        fenetre.after(15,alienMissile)
+    # Collision avec le bord haut
+    if misAlY > hauteur - 10:
+        Canevas.delete(misAlien)
+    # Collision avec le vaisseau
+    if misAlX > PosX - tailleVaiss and misAlX < PosX + tailleVaiss and misAlY > PosY - tailleVaiss and misAlY < PosY + tailleVaiss:
+        Canevas.delete('all')
+
+
 # Permet de gérer déplacement du vaisseau
-def GestionVaisseau(event):
-    global PosX,Largeur,tailleVaiss,missile,misX,misY
+def gestionVaisseau(event):
+    global PosX,largeur,tailleVaiss,missile,misX,misY,X,Y,misAlien,misAlX,misAlY
     touche = event.keysym
     # Vaisseau au milieu
-    if PosX+tailleVaiss < Largeur and PosX-tailleVaiss > 0:
+    if PosX+tailleVaiss < largeur and PosX-tailleVaiss > 0:
         # Déplacement à droite
         if touche == 'Right':
             PosX += 5
@@ -78,19 +98,23 @@ def GestionVaisseau(event):
         if touche == 'Left':
             PosX -= 5
     # Vaisseau sur le bord droit
-    elif PosX+tailleVaiss >= Largeur :
+    elif PosX+tailleVaiss >= largeur :
         if touche == 'Left':
             PosX -= 5
     # Vaisseau sur le bord gauche
     elif PosX-tailleVaiss < tailleVaiss:
         if touche == 'Right':
             PosX += 5
-        # on dessine le Vaisseau à sa nouvelle position
+    # on dessine le Vaisseau à sa nouvelle position
     Canevas.coords(Vaisseau,PosX -tailleVaiss, PosY -tailleVaiss, PosX +tailleVaiss, PosY +tailleVaiss)
     if touche == 'space':
         missile = Canevas.create_line(PosX,PosY,PosX,PosY-tailleMissile,fill='white')
         misX,misY=PosX,PosY
-        Deplacementmissile()
+        deplacementMissile()
+    if touche == 'a':
+        misAlX,misAlY=X[randint(0,len(X)-1)],Y
+        misAlien = Canevas.create_line(misAlX,misAlY,misAlX,misAlY-tailleMissile,fill='white')
+        alienMissile()
 
 
 
@@ -101,9 +125,9 @@ fenetre = Tk()
 fenetre.title('Space Invaders')
 
 # Zone principale de jeu :
-Canevas = Canvas(fenetre, width = Largeur, height = Hauteur, bg='grey')
+Canevas = Canvas(fenetre, width = largeur, height = hauteur, bg='grey')
 
-Canevas.bind('<Key>',GestionVaisseau)
+Canevas.bind('<Key>',gestionVaisseau)
 Canevas.pack(side = 'left')
 
 # Zone affichant le score :
@@ -132,5 +156,6 @@ alien = []
 for i in range(len(X)):
     alien.append(Canevas.create_rectangle(X[i]-RAYON,Y-RAYON,X[i]+RAYON,Y+RAYON,width=1,fill='blue'))
 Vaisseau = Canevas.create_rectangle(PosX-tailleVaiss,PosY-tailleVaiss,PosX+tailleVaiss,PosY+tailleVaiss,width=1,outline='black',fill='red')
+
 
 fenetre.mainloop()
